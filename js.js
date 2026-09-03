5937b4431006db467eebd48bfd26694eada41f88
gxuohcscchbsp <mateusz.domagala.08@gmail.com>
2026-09-03 22:11:12 +0200

    mydlo w plynie

---
+++ js.js
@@ -0,0 +1,20 @@
+function moveto(a){
+    if(a==1){
+        window.location.href = "learning/html.html";
+    }
+    if(a==2){
+        window.location.href = "css.html";
+    }
+    if(a==3){
+        window.location.href = "learning/javascript.html";
+    }
+    if(a==4){
+        window.location.href = "learning/php.html";
+    }
+    if(a==5){
+        window.location.href = "learning/mysql.html";
+    }
+    if(a==6){
+        window.location.href = "learning/cpp.html";
+    }
+}
